a98971a7b974dc62305745150061bbae8c54672d
Dhruv <dhruvrawat105@gmail.com>
2021-10-08 23:11:59 +0530

    first commit

---
+++ routes/keys.js
@@ -0,0 +1,15 @@
+const GOOGLE = {
+    clientID: "792595948741-gt4mskg6bjhfb114u91jer0spqvufajm.apps.googleusercontent.com",
+    clientSecret: "uHjRV9B_CWK7t0gH6VGuUvTB"
+};
+
+
+
+const API_URL = 'https://api.themoviedb.org/3/';
+const API_KEY = '33bb8436c172b0643a6db970caa59640';
+
+module.exports = {
+    API_URL,
+    API_KEY,
+    GOOGLE,
+};
